d539df0df88547361d58d0dedf3a5f1d903f7570
vineeth-0509 <vineeththungani@gmail.com>
2024-09-23 20:57:23 +0530

    Install Reactjs and tailwindcss and create 1St template

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',  // Make sure your file extensions and paths are covered
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
