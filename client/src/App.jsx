fd9f4f7b8707608e2316be361d41910a4de5dcfb
vineeth-0509 <vineeththungani@gmail.com>
2024-09-29 16:55:52 +0530

    update the header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/sign-in" element={<Signin/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
+      <Route  element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile/>}/>
+      </Route>
       <Route path="/about" element={<About/>}/>
     </Routes>
     </BrowserRouter>
