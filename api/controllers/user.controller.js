b238eec342d04a3ce2001b407b1c677a6353487d
vineeth-0509 <vineeththungani@gmail.com>
2024-10-01 22:35:21 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -29,3 +29,15 @@ export const updateUser = async (req,res) => {
             next(error)
         }
 }
+
+
+export const deleteUser =async () => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401,'You can only delete yout own account'))
+        try {
+           await User.findByIdAndDelete(req.params.id);
+           res.clearCookie('access_token');
+           res.status(200).json('User has been deleted!');
+        } catch (error) {
+            next(error);
+        }
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,7 +6,9 @@ import { app } from '../firebase';
 import {
   updateUserStart,
   updateUserSuccess,
-  updateUserFailure
+  updateUserFailure,
+  deleteUserFailure,
+  deleteUserSuccess
 } from "../redux/user/userSlice.js";
 import { useDispatch } from 'react-redux';
 
@@ -77,6 +79,24 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   }
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: "DELETE",
+      })
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message));
+        retturn;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -112,7 +132,7 @@ export default function Profile() {
           </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete Account </span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account </span>
         <span className='text-red-700 cursor-pointer'>Sign Out </span>
       </div>
 
